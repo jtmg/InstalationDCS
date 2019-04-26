444f24033dce9a495783b2a30e270ea0ac3383d8
José Gouveia <jose.t.m.gouveia@gmail.com>
2019-04-26 20:37:09 +0100

    remove tecnologies

--- instalation/installation.ps1
+++ instalation/installation.ps1
@@ -8,14 +8,11 @@ $currentPAth = $PSScriptRoot.ToString() + "\Utils.ps1"
 .$currentPAth
 
 InitScoop 
-#Install-Program ("mongodb")
 #Install-Program ("eclipse-java")
-#Install-Program ("mysql")
 Install-Program ("heidisql")
 Install-Program ("robo3t")
 #Install-Program ("maven")
-#Install-Program ("wildFly.json")
-#Install-Program ("robo3t")
+
 
 
 docker run --name mysql -d -it -p 3306:3306 asseco/mysql
