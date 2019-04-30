58cced52984b98cb6a654cdedca9b408dab5e3b4
José Gouveia <jose.t.m.gouveia@gmail.com>
2019-04-30 21:20:35 +0100

    update instalation script

--- instalation/installation.ps1
+++ instalation/installation.ps1
@@ -15,6 +15,8 @@ Install-Program ("robo3t")
 
 #docker login -u admin -p admin123 34.250.203.252:8082
 #docker login -u admin -p admin123 34.250.203.252:8083
+docker pull 3306:3306 34.250.203.252:8082/mysql
+docker pull 3306:3306 34.250.203.252:8082/mongo
 
 docker run --name mysql -d -it -p 3306:3306 34.250.203.252:8082/mysql
 docker run --name mongo -d -it -p 27017:27017 34.250.203.252:8082/mongo
